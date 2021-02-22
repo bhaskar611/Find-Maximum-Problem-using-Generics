9f6ad2803c9633f53e0bd1589dab4ed07de614e0
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-02-22 19:12:19 +0530

    Added:Welcome Message

---
+++ Generics_Test_Maximum/src/main/java/com/Generics/Generics_Test_Maximum/FindMaximum.java
@@ -0,0 +1,11 @@
+package com.Generics.Generics_Test_Maximum;
+
+
+
+public class FindMaximum 
+{
+	public static void printWelcomeMessage() {
+		System.out.println("****Welcome to the Generics Test Maximum Program******");
+	}
+
+}
