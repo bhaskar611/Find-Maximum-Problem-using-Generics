73581bf188d5d0541587b0ee787029d09f33e8fb
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-02-25 16:17:39 +0530

    Added: Refactor the code to one Generic method

--- Generics_Test_Maximum/src/test/java/com/Generics/Generics_Test_Maximum/FindMaximumTest.java
+++ Generics_Test_Maximum/src/test/java/com/Generics/Generics_Test_Maximum/FindMaximumTest.java
@@ -6,63 +6,58 @@ public class FindMaximumTest {
 	
 	
 	@Test
-	public void maximumIntegerAtPosition1() {
-		Integer maximumInteger = FindMaximum.maximumInteger(12, 4, 11);
-		Assert.assertSame(12, maximumInteger);
+	public void maximumObjectIntegerposition1() {
+		Integer maximumObject = FindMaximum.maximumOfObject(999, 99, 9);
+		
+		Assert.assertEquals((Integer) 999, maximumObject);
+	}
+	@Test
+	public void maximumObjectIntegerposition2() {
+		Integer maximumObject = FindMaximum.maximumOfObject(99, 999, 9);
+		
+		Assert.assertEquals((Integer) 999, maximumObject);
+	}
+	@Test
+	public void maximumObjectIntegerposition3() {
+		Integer maximumObject = FindMaximum.maximumOfObject(9, 99,999);
+		
+		Assert.assertEquals((Integer) 999, maximumObject);
 	}
 
 	
 	@Test
-	public void maximumIntegerAtPosition2() {
-		Integer maximumInteger = FindMaximum.maximumInteger(4, 12, 11);
-		Assert.assertSame(12, maximumInteger);
+	public void maximumObjectFloatposition1() {
+		Float maximumObject = FindMaximum.maximumOfObject(15.69f, 12.36f, 6.96f);
+		Assert.assertEquals((Float) 15.69f, maximumObject);
+	}
+	
+	@Test
+	public void maximumObjectFloatposition2() {
+		Float maximumObject = FindMaximum.maximumOfObject(12.36f, 15.69f, 6.96f);
+		Assert.assertEquals((Float) 15.69f, maximumObject);
+	}
+	@Test
+	public void maximumObjectFloatposition3() {
+		Float maximumObject = FindMaximum.maximumOfObject(12.36f, 6.96f, 15.69f);
+		Assert.assertEquals((Float) 15.69f, maximumObject);
 	}
 
 	
 	@Test
-	public void maximumIntegerAtPosition3() {
-		Integer maximumInteger = FindMaximum.maximumInteger(4, 11, 12);
-		Assert.assertSame(12, maximumInteger);
+	public void maximumObjectStringposition1() {
+		String maximumObject = FindMaximum.maximumOfObject("peach", "apple", "banana");
+		Assert.assertEquals((String) "peach", maximumObject);
 	}
-	
-	
 	@Test
-	public void maximumFloatAtPosition1() {
-		Float maximumFloat = FindMaximum.maximumFloat(4.2f, 3.1f, 1.5f);
-		Assert.assertEquals((Float) 4.2f, maximumFloat);
+	public void maximumObjectStringposition2() {
+		String maximumObject = FindMaximum.maximumOfObject("peach", "apple", "banana");
+		Assert.assertEquals((String) "peach", maximumObject);
 	}
-
-	
 	@Test
-	public void maximumFloatAtPosition2() {
-		Float maximumFloat = FindMaximum.maximumFloat(3.1f, 4.2f, 1.5f);
-		Assert.assertEquals((Float) 4.2f, maximumFloat);
-	}
-
-	
-	@Test
-	public void maximumFloatAtPosition3() {
-		Float maximumFloat = FindMaximum.maximumFloat(1.5f, 3.1f, 4.2f);
-		Assert.assertEquals((Float) 4.2f, maximumFloat);
+	public void maximumObjectStringposition3() {
+		String maximumObject = FindMaximum.maximumOfObject("peach", "apple", "banana");
+		Assert.assertEquals((String) "peach", maximumObject);
 	}
 	
-	@Test
-	public void maximumStringAtPosition1() {
-		String maximumString = FindMaximum.maximumString("Peach", "Apple", "Banana");
-		Assert.assertEquals("Peach", maximumString);
-	}
-
 	
-	@Test
-	public void maximumStringAtPosition2() {
-		String maximumString = FindMaximum.maximumString("Apple", "Peach", "Banana");
-		Assert.assertEquals("Peach", maximumString);
-	}
-
-	
-	@Test
-	public void maximumStringAtPosition3() {
-		String maximumString = FindMaximum.maximumString("Apple", "Banana", "Peach");
-		Assert.assertEquals("Peach", maximumString);
-	}
 }
